1d123a357a3b508d5ad8fd73222536f79356b375
efrainceh <efrain.ceh@gmail.com>
2024-03-27 14:48:53 -0400

    Key to iconbar

--- app/components/profile/IconBar.tsx
+++ app/components/profile/IconBar.tsx
@@ -23,15 +23,14 @@ export default function IconBar() {
   const icons: React.JSX.Element[] = [];
   for (let ix = 0; ix < hrefs.length; ix++) {
     icons.push(
-      <React.Fragment key={texts[ix]}>
-        <Icon
-          internal={internal[ix]}
-          href={hrefs[ix]}
-          target={targets[ix]}
-          iconName={iconNames[ix]}
-          text={texts[ix]}
-        />
-      </React.Fragment>
+      <Icon
+        key={texts[ix]}
+        internal={internal[ix]}
+        href={hrefs[ix]}
+        target={targets[ix]}
+        iconName={iconNames[ix]}
+        text={texts[ix]}
+      />
     );
   }
   return <div className={styles.iconBar}>{icons}</div>;
