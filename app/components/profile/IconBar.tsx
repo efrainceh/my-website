ee9e8cccbef3e5bd52721ed25980c226a7596167
efrainceh <efrain.ceh@gmail.com>
2024-03-27 15:37:53 -0400

    Added key to iconNames

--- app/components/profile/IconBar.tsx
+++ app/components/profile/IconBar.tsx
@@ -13,27 +13,26 @@ export default function IconBar() {
   ];
   const targets = ["_blank", "_blank", "_blank", "_self"];
   const iconNames = [
-    <SiLinkedin size="2em" />,
-    <SiGithub size="2em" />,
-    <SiReaddotcv size="2em" />,
-    <SiFiles size="2em" />,
+    <SiLinkedin key={0} size="2em" />,
+    <SiGithub key={1} size="2em" />,
+    <SiReaddotcv key={2} size="2em" />,
+    <SiFiles key={3} size="2em" />,
   ];
   const texts = ["LinkedIn", "Github", "Resume", "Projects"];
 
-  // const icons: React.JSX.Element[] = [];
-  // for (var ix = 0; ix < hrefs.length; ix++) {
-  //   icons.push(
-  //     <h1 key={ix}>texts[ix]</h1>
-  //     // <Icon
-  //     //   key={texts[ix]}
-  //     //   internal={internal[ix]}
-  //     //   href={hrefs[ix]}
-  //     //   target={targets[ix]}
-  //     //   iconName={iconNames[ix]}
-  //     //   text={texts[ix]}
-  //     // />
-  //   );
-  // }
+  const icons: React.JSX.Element[] = [];
+  for (var ix = 0; ix < hrefs.length; ix++) {
+    icons.push(
+      <Icon
+        key={texts[ix]}
+        internal={internal[ix]}
+        href={hrefs[ix]}
+        target={targets[ix]}
+        iconName={iconNames[ix]}
+        text={texts[ix]}
+      />
+    );
+  }
 
   return <div className={styles.iconBar}></div>;
 }
