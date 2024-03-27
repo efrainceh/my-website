bb8e7a23a7fc3557609346f86d056e6034b62e28
efrainceh <efrain.ceh@gmail.com>
2024-03-27 15:34:31 -0400

    t4

--- app/components/profile/IconBar.tsx
+++ app/components/profile/IconBar.tsx
@@ -20,20 +20,20 @@ export default function IconBar() {
   ];
   const texts = ["LinkedIn", "Github", "Resume", "Projects"];
 
-  const icons: React.JSX.Element[] = [];
-  for (var ix = 0; ix < hrefs.length; ix++) {
-    icons.push(
-      <h1 key={ix}>texts[ix]</h1>
-      // <Icon
-      //   key={texts[ix]}
-      //   internal={internal[ix]}
-      //   href={hrefs[ix]}
-      //   target={targets[ix]}
-      //   iconName={iconNames[ix]}
-      //   text={texts[ix]}
-      // />
-    );
-  }
+  // const icons: React.JSX.Element[] = [];
+  // for (var ix = 0; ix < hrefs.length; ix++) {
+  //   icons.push(
+  //     <h1 key={ix}>texts[ix]</h1>
+  //     // <Icon
+  //     //   key={texts[ix]}
+  //     //   internal={internal[ix]}
+  //     //   href={hrefs[ix]}
+  //     //   target={targets[ix]}
+  //     //   iconName={iconNames[ix]}
+  //     //   text={texts[ix]}
+  //     // />
+  //   );
+  // }
 
-  return <div className={styles.iconBar}>{icons}</div>;
+  return <div className={styles.iconBar}></div>;
 }
